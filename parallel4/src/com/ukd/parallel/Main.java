4902be0b725f660d02ec724387c8d9ceb2ae9ed0
sofijasljusar <sofija.sljusar@gmail.com>
2025-10-27 10:11:19 +0200

    Complete Practice 4

---
+++ parallel4/src/com/ukd/parallel/Main.java
@@ -0,0 +1,41 @@
+package com.ukd.parallel;
+
+public class Main {
+
+     static class Worker implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    System.out.println("Worker is running...");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    System.out.println("Worker interrupted. Exiting...");
+                    break;
+                }
+
+                if (Thread.currentThread().isInterrupted()) {
+                    System.out.println("Worker detected interrupt. Exiting...");
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread workerThread = new Thread(new Worker(), "WorkerThread");
+        workerThread.start();
+
+        Thread.sleep(5000);
+
+        System.out.println("Main thread: interrupting worker...");
+        workerThread.interrupt();
+
+        workerThread.join();
+        System.out.println("Worker thread has finished.");
+    }
+
+
+}
